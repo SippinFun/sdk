3e15c2573658ca195140e9ddcbd136ce93c0af88
Sippin <akufifa121@gmail.com>
2026-03-08 21:37:12 +0700

    Create config.js

---
+++ src/config/config.js
@@ -0,0 +1,8 @@
+require("dotenv").config();
+
+module.exports = {
+  projectName: "Sippin",
+  description: "AI agent powered by meme coin culture",
+  twitter: "https://x.com/Sippinfun",
+  website: "https://sippin.fun/",
+};
